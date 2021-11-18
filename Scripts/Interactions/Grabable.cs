cde20a082b0503bd27e085b580bb52bf638137a5
nailuj05 <julian.limburg@gmx.de>
2021-11-18 17:56:59 +0100

    Moved Grabable Update to Fixed Update

--- Scripts/Interactions/Grabable.cs
+++ Scripts/Interactions/Grabable.cs
@@ -43,7 +43,7 @@ namespace Fusion.XR
             rb = GetComponent<Rigidbody>();
         }
 
-        public virtual void Update()
+        public virtual void FixedUpdate()
         {
             if (!isGrabbed)
                 return;
