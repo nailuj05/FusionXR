7b1d8446a9ff75caca45d40e6454b77bdc7aa1eb
nailuj05 <julian.limburg@gmx.de>
2021-11-19 19:01:13 +0100

    Fixed wheel

--- Scripts/Interactions/Interactables/KinematicWheel.cs
+++ Scripts/Interactions/Interactables/KinematicWheel.cs
@@ -6,29 +6,23 @@ namespace Fusion.XR
 {
     public class KinematicWheel : KinematicInteractable
     {
-        private Vector3 localGrabPosition = Vector3.zero;
-
-        public float angle = 0f;
+        private Vector3 grabPosition = Vector3.zero;
+        private float offsetAngle = 0f;
 
         protected override void InteractionStart()
         {
             isInteracting = true;
 
-            localGrabPosition = transform.InverseTransformPoint(attachedHands[0].grabPosition.position);
-            angle = 0f;
+            grabPosition = attachedHands[0].grabPosition.position;
+
+            offsetAngle = Vector3.SignedAngle(LocalAngleSetup(attachedHands[0].targetPosition), LocalAngleSetup(grabPosition), axis);
         }
 
         protected override void InteractionUpdate()
         {
-            var targetPos = transform.TransformPoint(localGrabPosition);
-            var deltaAngle = Vector3.SignedAngle(LocalAngleSetup(attachedHands[0].targetPosition), LocalAngleSetup(targetPos), axis);
+            var deltaAngle = offsetAngle - Vector3.SignedAngle(LocalAngleSetup(attachedHands[0].targetPosition), LocalAngleSetup(grabPosition), axis);
 
-            angle = Mathf.MoveTowardsAngle(angle, deltaAngle, 2f);
-
-            //transform.rotation = Quaternion.Euler(transform.InverseTransformDirection(axis) * deltaAngle);
-            transform.rotation *= Quaternion.Euler(transform.InverseTransformDirection(axis) * angle);
-            //transform.Rotate(transform.InverseTransformDirection(axis), deltaAngle, Space.Self);
-            //transform.Rotate(axis, deltaAngle, Space.World);
+            transform.localEulerAngles = axis * deltaAngle;
         }
 
         protected override void InteractionEnd()
@@ -42,27 +36,3 @@ namespace Fusion.XR
         }
     }
 }
-
-//if (attachedHands.Count == 1) //If there is one hand grabbing
-//{
-//    //Get GrabPoint Offsets
-//    Vector3 offsetPos = attachedHands[0].grabPosition.localPosition;
-
-//    //Delta Vector from Grabable (+ offset) to hand
-//    targetPosition = attachedHands[0].targetPosition - transform.TransformVector(offsetPos);
-//}
-//else //If there is two hands grabbing 
-//{
-//    Vector3[] posTargets = new Vector3[attachedHands.Count];
-
-//    for (int i = 0; i < attachedHands.Count; i++)
-//    {
-//        //Get GrabPoint Offsets
-//        Vector3 offsetPos = attachedHands[i].grabPosition.localPosition;
-
-//        //Delta Vector from Grabable (+ offset) to hand
-//        posTargets[i] = attachedHands[i].targetPosition - transform.TransformVector(offsetPos);
-//    }
-//    //Average target transformation
-//    targetPosition = Vector3.Lerp(posTargets[0], posTargets[1], 0.5f);
-//}
