f9f6426320ea76c8fd4657518aa4332d73654518
nailuj05 <julian.limburg@gmx.de>
2021-12-17 21:14:36 +0100

    Stabbing now impales, collision exit broken

--- Scripts/Interactions/Stabber.cs
+++ Scripts/Interactions/Stabber.cs
@@ -9,13 +9,40 @@ namespace Fusion.XR
     {
         public float requiredImpactVelocity;
 
+        private Rigidbody rb;
+
+        private float unstabTime = 0.3f;
+
         private ConfigurableJoint stabJoint;
+        private float stabTime;
+        private GameObject stabbedObject;
+        private Collider[] colliders;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            colliders = GetComponents<Collider>();
+        }
 
         private void OnCollisionEnter(Collision collision)
         {
             if(collision.relativeVelocity.magnitude > requiredImpactVelocity)
             {
+                Debug.Log("Attach Joint");
+
+                IgnoreCollisions(collision.gameObject.GetComponents<Collider>(), true);
                 AttachJoint(collision.collider.gameObject);
+                stabTime = Time.time;
+            }
+        }
+
+        //This is not being called if Ignore Collision is enabled
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.gameObject == stabbedObject)
+            {
+                Debug.Log($"Exit {collision.collider}");
+                DetachJoint();
             }
         }
 
@@ -24,11 +51,38 @@ namespace Fusion.XR
             stabJoint = gameObject.AddComponent<ConfigurableJoint>();
 
             stabJoint.angularXMotion = stabJoint.angularYMotion = stabJoint.angularZMotion = ConfigurableJointMotion.Locked;
-            stabJoint.xMotion = stabJoint.yMotion = stabJoint.zMotion = ConfigurableJointMotion.Locked;
+            stabJoint.yMotion = stabJoint.zMotion = ConfigurableJointMotion.Locked;
+
+            stabbedObject = objectToStab;
 
             if (objectToStab.TryGetComponent(out Rigidbody rigidbodyToStab))
             {
                 stabJoint.connectedBody = rigidbodyToStab;
+            }
+        }
+
+        void DetachJoint()
+        {
+            Debug.Log($"Try detach: {(Time.time - stabTime)}");
+
+            if((Time.time - stabTime) > unstabTime)
+            {
+                Debug.Log("Detach Joint");
+                IgnoreCollisions(stabbedObject.GetComponents<Collider>(), false);
+
+                stabbedObject = null;
+                Destroy(stabJoint);
+            }
+        }
+
+        void IgnoreCollisions(Collider[] stabbedColliders, bool ignore)
+        {
+            foreach(Collider coll in colliders)
+            {
+                foreach (Collider stabColl in stabbedColliders)
+                {
+                    Physics.IgnoreCollision(coll, stabColl, ignore);
+                }
             }
         }
     }
