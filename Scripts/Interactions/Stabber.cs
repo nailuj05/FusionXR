51eee7c54285e7e231063db5b941abb7f9a2c634
nailuj05 <julian.limburg@gmx.de>
2021-12-17 17:03:06 +0100

    What is this trash Basic implementation done

--- Scripts/Interactions/Stabber.cs
+++ Scripts/Interactions/Stabber.cs
@@ -7,95 +7,29 @@ namespace Fusion.XR
 {
     public class Stabber : MonoBehaviour
     {
-        public Transform stabColliderCenter;
-        public Vector3 stabColliderDimensions = Vector3.one / 20;
-
         public float requiredImpactVelocity;
 
-        private float lastImpactVelocity;
-
-        private GameObject stabbedObject;
         private ConfigurableJoint stabJoint;
-        private Collider[] hitColliders;
 
-        //private void OnCollisionEnter(Collision collision)
-        //{
-        //    //Calculate Impact Velocity relative to knifes stab axis
-        //    lastImpactVelocity = Vector3.Project(collision.relativeVelocity, stabColliderCenter.forward).sqrMagnitude;
-
-        //    //Check if impact velocity is large enough
-        //    if (lastImpactVelocity >= requiredImpactVelocity)
-        //    {
-        //        //Check if there is a collision at the stab collider
-        //        hitColliders = Physics.OverlapBox(stabColliderCenter.position, stabColliderDimensions, stabColliderCenter.rotation);
-
-        //        if (hitColliders.Length > 0)
-        //        {
-        //            //Attach joint to the first collider (calculating the closest is to performance heavy)
-        //            AttachJoint(hitColliders[0].gameObject);
-        //        }
-        //    }
-        //}
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.relativeVelocity.magnitude > requiredImpactVelocity)
+            {
+                AttachJoint(collision.collider.gameObject);
+            }
+        }
 
         void AttachJoint(GameObject objectToStab)
         {
-            //Add the Joint
-            stabJoint = objectToStab.AddComponent<ConfigurableJoint>();
+            stabJoint = gameObject.AddComponent<ConfigurableJoint>();
 
-            //If the object has a rigidbody
+            stabJoint.angularXMotion = stabJoint.angularYMotion = stabJoint.angularZMotion = ConfigurableJointMotion.Locked;
+            stabJoint.xMotion = stabJoint.yMotion = stabJoint.zMotion = ConfigurableJointMotion.Locked;
+
             if (objectToStab.TryGetComponent(out Rigidbody rigidbodyToStab))
             {
                 stabJoint.connectedBody = rigidbodyToStab;
-                stabJoint.anchor = objectToStab.transform.TransformPoint(stabColliderCenter.position);
-            }
-            //If the object has no rigidbody
-            else
-            {
-                stabJoint.anchor = objectToStab.transform.TransformPoint(stabColliderCenter.position);
-            }
-        }
-
-        public void SetupStabCollider()
-        {
-            if (stabColliderCenter)
-                return;
-
-            GameObject coll = new GameObject("StabCollider");
-            coll.transform.parent = transform;
-            coll.transform.localPosition = Vector3.zero;
-
-            stabColliderCenter = coll.transform;
-        }
-
-#if UNITY_EDITOR
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = new Color(0, 0, 1, 0.4f);
-            Gizmos.DrawCube(stabColliderCenter.position, stabColliderDimensions);
-        }
-#endif
-    }
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(Stabber))] [CanEditMultipleObjects]
-    public class StabberEditor : Editor
-    {
-        Stabber stabber;
-
-        private void Awake()
-        {
-            stabber = (Stabber)target;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            if(GUILayout.Button("AddStabCollider"))
-            {
-                stabber.SetupStabCollider();
             }
         }
     }
-#endif
 }
