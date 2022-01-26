b29f71a1821e70668e4b0a7cd2b921f461645d25
nailuj05 <julian.limburg@gmx.de>
2022-01-26 12:54:53 +0100

    Added Kinematic Slider
    (cherry picked from commit 7b09b9ec842e9ca4bc82508df6deda76176560d4)

---
+++ Scripts/Interactions/Interactables/KinematicSlider.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Fusion.XR
+{
+    public class KinematicSlider : KinematicInteractable
+    {
+        public Vector3 lineStart;
+        public Vector3 lineEnd;
+
+        public Vector3 lineStartGlobal { get { return transform.parent.TransformPoint(lineStart); } }
+        public Vector3 lineEndGlobal { get { return transform.parent.TransformPoint(lineEnd); } }
+
+        private Vector3 globalLinePoint => transform.parent.TransformPoint((lineStart + lineEnd) * 0.5f);
+        private Vector3 globalLineDir => transform.parent.TransformDirection(lineEnd - lineStart);
+
+        private float globalLineLength => Vector3.Distance(transform.parent.TransformVector(lineStart), transform.parent.TransformVector(lineEnd));
+
+        protected override void InteractionStart()
+        {
+            if (attachedHands.Count == 0) return;
+
+            isInteracting = true;
+        }
+
+        protected override void InteractionUpdate()
+        {
+            transform.position = Utils.ClosestPointOnLine(globalLinePoint, globalLineDir, globalLineLength, attachedHands[0].targetPosition);
+        }
+
+        protected override void InteractionEnd()
+        {
+            isInteracting = false;
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(lineStartGlobal, lineEndGlobal);
+        }
+#endif
+    }
+}
