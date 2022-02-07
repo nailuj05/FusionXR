fa5a66d504e17a2d4714e7cdfff719d13529d179
nailuj05 <julian.limburg@gmx.de>
2022-02-07 19:33:56 +0100

    Kinematic Button no longer needs DoTween

--- Scripts/Interactions/Interactables/KinematicButton.cs
+++ Scripts/Interactions/Interactables/KinematicButton.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 namespace Fusion.XR
 {
@@ -13,23 +11,47 @@ namespace Fusion.XR
         private float buttonMoveTime = 1;
 
         private Vector3 initialPosition;
+        private Vector3 pressPosition;
 
         private void Start()
         {
             initialPosition = transform.localPosition;
+            pressPosition = initialPosition + axis * pressDistance;
         }
 
         protected override void InteractionStart()
         {
             //Release hand immediatly?
-            Sequence s = DOTween.Sequence();
+            StopAllCoroutines();
 
-            s.Append(transform.DOLocalMove(initialPosition + axis * pressDistance, buttonMoveTime / 2));
-            s.Append(transform.DOLocalMove(initialPosition, buttonMoveTime / 2));
-            s.SetLoops(1);
-            s.Play();
+            StartCoroutine(AnimateButtons());
 
             isInteracting = true;
+        }
+
+        IEnumerator AnimateButtons()
+        {
+            float lerp = 0;
+            Vector3 startPosition = transform.localPosition;
+            float speedAdj = Vector3.Distance(startPosition, pressPosition) / Mathf.Abs(pressDistance);
+
+            while(lerp < 1)
+            {
+                lerp += Time.deltaTime / (buttonMoveTime * 0.5f) / speedAdj;
+                transform.localPosition = Vector3.Lerp(startPosition, pressPosition, lerp);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            lerp = 0;
+
+            while (lerp < 1)
+            {
+                lerp += Time.deltaTime / (buttonMoveTime * 0.5f);
+                transform.localPosition = Vector3.Lerp(pressPosition, initialPosition, lerp);
+
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         protected override void InteractionUpdate() { }
