c1da07a5af489c304034acbc2e7e659bc7fe1219
nailuj05 <julian.limburg@gmx.de>
2022-03-01 09:20:19 +0100

    Work on Joystick

--- Scripts/Interactions/Interactables/KinematicJoystick.cs
+++ Scripts/Interactions/Interactables/KinematicJoystick.cs
@@ -6,9 +6,18 @@ namespace Fusion.XR
 {
     public class KinematicJoystick : KinematicInteractable
     {
+        public float Rad = 10;
+
         private Vector3 grabPosition = Vector3.zero;
 
         private Vector3 initialAxis;
+
+        private Quaternion initialRot;
+
+        private void Awake()
+        {
+            initialRot = transform.localRotation;
+        }
 
         protected override void InteractionStart()
         {
@@ -18,12 +27,18 @@ namespace Fusion.XR
 
             grabPosition = attachedHands[0].grabPosition.position;
 
-            initialAxis = transform.parent.InverseTransformDirection(transform.TransformDirection(axis));
+            initialAxis = transform.parent.InverseTransformDirection(transform.TransformDirection(axis)).normalized;
         }
 
         protected override void InteractionUpdate()
         {
-            
+            var newAxis = GetMeanPosition() - transform.position;
+            //newAxis = transform.parent.InverseTransformDirection(newAxis);
+            newAxis.Normalize();
+
+            //newAxis = Vector3.MoveTowards(initialAxis, newAxis, Rad);
+
+            transform.localRotation = initialRot * Quaternion.FromToRotation(transform.parent.TransformDirection(initialAxis), newAxis);
         }
 
         protected override void InteractionEnd()
