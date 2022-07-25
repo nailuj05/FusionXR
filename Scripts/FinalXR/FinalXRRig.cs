bbbb1f6c5b9ce244d55f114120694f064ed62f39
Julian <julian.limburg@gmx.de>
2022-07-26 01:09:27 +0200

    Final Rig

---
+++ Scripts/FinalXR/FinalXRRig.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalXRRig : MonoBehaviour
+{
+    public Rigidbody head;
+    public Rigidbody locosphere;
+
+    public Transform h;
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
